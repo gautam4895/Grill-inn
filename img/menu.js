4b0fcd98cc705de768c5f61b7fd0a3eeec0a835b
gautam <monur4895@gmail.com>
2021-07-25 09:09:01 +0530

    Update menu.js

--- img/menu.js
+++ img/menu.js
@@ -283,30 +283,30 @@
 "Pastas": [
     {
         "id": 0,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Veg Penne Arabiata",
+        "price": "109",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Pastas/VEG-PENNE-ARRABBIATA.png?token=AQZAIB5J7PHPGBLUTUJ4H2TBAYLE2",
         "non-veg": false
     },
     {
         "id": 1,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Veg Creamy White",
+        "price": "109",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Pastas/VEG-CREAMY-WHITE.png?token=AQZAIB26GHP6AK4NH247MSLBAYLFM",
         "non-veg": false
     },
     {
         "id": 2,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Chicken Penne Arabiata",
+        "price": "119",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Pastas/CHK-PENNE-ARRABBIATA.png?token=AQZAIB2JIFGFKISII6ABEN3BAYLFW",
         "non-veg": true
     },
     {
         "id": 3,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Chicken Creamy White COMPOS",
+        "price": "119",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Pastas/CHK-CREAMY-WHITE.png?token=AQZAIB3BZ57GMD47LCNG4F3BAYLGA",
         "non-veg": true
     }
 ]
