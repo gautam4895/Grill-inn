ee1ac7000e99c14d770ec6e9dd6f82567e336df0
gautam <monur4895@gmail.com>
2021-07-25 10:07:36 +0530

    img

--- img/menu.js
+++ img/menu.js
@@ -312,44 +312,44 @@
 "Sandwiches": [
     {
         "id": ,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Veg Grilled Sandwich",
+        "price": "79",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Sandwiches/VEG-GRILLED-Sandwiches.png?token=AQZAIBZZOQNGAX4G7PUWXSDBAYMFQ",
         "non-veg": false
     },
     {
         "id": ,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Paneer Tikka Sandwich",
+        "price": "99",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Sandwiches/PANEER-TIKKA-SANDWICH.png?token=AQZAIBYUEOAO2LSGHJJYLVDBAYPPY",
         "non-veg": false
     },
     {
         "id": ,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Italian Veg Sandwich",
+        "price": "109",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Sandwiches/ITALIAN-VEG-SANDWICH.png?token=AQZAIBYR7BKJNVC6KJ5JMGTBAYPR2",
         "non-veg": false
     },
     {
         "id": ,
-        "name": "",
+        "name": "Chicken Grilled Sandwich",
+        "price": "89",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Sandwiches/CHICKEN-TIKKA-SANDWICH.png?token=AQZAIBYY2X232OUSQUEJJULBAYPUK",
+        "non-veg": true
+    },
+    {
+        "id": ,
+        "name": "Chicken Tikka Sandwich",
         "price": "",
         "img": "",
         "non-veg": true
     },
     {
         "id": ,
-        "name": "",
-        "price": "",
-        "img": "",
-        "non-veg": true
-    },
-    {
-        "id": ,
-        "name": "",
-        "price": "",
-        "img": "",
+        "name": "Italian Chicken Sandwich",
+        "price": "109",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Sandwiches/ITALIAN-CHICKEN-SANDWICH.png?token=AQZAIB6EC4NSBOHE37J44RLBAYPS4",
         "non-veg": true
     }
 ]
