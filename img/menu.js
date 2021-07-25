9ecc279dd7d0f2d907c17ac3948d25aba7817488
gautam <monur4895@gmail.com>
2021-07-25 19:12:32 +0530

    Update menu.js

--- img/menu.js
+++ img/menu.js
@@ -588,21 +588,21 @@
         "id": 3,
         "name": "Shakes",
         "price": "89",
-        "img": "",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Beverages/Shakes.png?token=AQZAIB7RWCEFAZ56VD2SDVDBAZ3W4",
         "non-veg": false
     },
     {
         "id": 4,
         "name": "Oreo Shakes",
         "price": "99",
-        "img": "",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Beverages/Oreo-Shake.png?token=AQZAIBYP645CLQX6NHJYJADBAZ3VM",
         "non-veg": false
     },
     {
         "id": 5,
         "name": "Kit Kat Shake",
         "price": "99",
-        "img": "",
+        "img": "https://raw.githubusercontent.com/gautam4895/Grill-inn/main/img/Beverages/Kitkat-Sshake.png?token=AQZAIB2DEH6XF6VSIO26KETBAZ3SY",
         "non-veg": false
     },
     {
